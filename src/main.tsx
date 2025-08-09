7c03fb2eba05ee42638bee560456eb348e5d0d53
Federico <ferrettife.018@gmail.com>
2025-08-10 01:13:50 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, redirect } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 import App from "./pages/App";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    loader: () => redirect(`/l/${uuidv4()}`),
+    loader: () => redirect(`/l/${crypto.randomUUID()}`),
   },
   { path: "/l/:listId", element: <App /> },
 ]);
